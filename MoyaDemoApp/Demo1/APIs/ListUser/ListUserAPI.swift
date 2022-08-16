069fdd49dc9688fbde40d19641fdad5a134126c1
JaeKi <jaeki@hu-mart.com>
2022-08-17 01:01:33 +0900

    comment

--- MoyaDemoApp/Demo1/APIs/ListUser/ListUserAPI.swift
+++ MoyaDemoApp/Demo1/APIs/ListUser/ListUserAPI.swift
@@ -11,11 +11,16 @@ struct ListUserAPI: Networkable {
     typealias Target = ListUserTarget
     
     static func getUserList(request: ListUserRequest, completion: @escaping (_ succeed: ListUser?, _ failed: Error?) -> Void) {
+        /*provider.request(tagetType) {
+         
+         }
+         */
         makeProvider().request(.list(request)) { result in
             switch ResponseData<ListUser>.processResponse(result) {
             case .success(let model): return completion(model, nil)
             case .failure(let error): return completion(nil, error)
             }
         }
+        
     }
 }
